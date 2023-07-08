db6ba47ba050bd06d9856750ac4220ff8683a738
WarokG <138694390+WarokG@users.noreply.github.com>
2023-07-08 16:45:52 +0200

    Update index.js

--- MHBuilds/index.js
+++ MHBuilds/index.js
@@ -3,11 +3,8 @@ const bot = new Client({intents: 3276799});
 const { token, botId } = require("./config.json");
 const handlerEvents = require ("./Handlers/handlerEvents");
 const loadSlashCommands = require ("./loadSlashCommands");
-const fs = require("fs");
-const path = require('node:path');
 
 bot.commands = new Collection();
-const folderPath = path.join(__dirname, "Commands");
 
 loadSlashCommands(bot);
 handlerEvents(bot);
