d00ab3041b6b6add2892d1246713f0057c02d6d5
WarokG <138694390+WarokG@users.noreply.github.com>
2023-07-08 16:43:58 +0200

    Update build.js

--- MHBuilds/Commands/build.js
+++ MHBuilds/Commands/build.js
@@ -31,15 +31,15 @@ module.exports = {
 
         if (arme === 'ls'){
             if (element === 'feu' || element === 'eau' || element === 'raw'){
-            await interaction.reply('https://cdn.discordapp.com/attachments/827646319656042536/994613711471919174/unknown.png');
+            await interaction.reply('${elemennt}');
             } else {
                 await interaction.reply('LS');
             }
 
         } else if (arme === 'gs'){
-            await interaction.reply('https://cdn.discordapp.com/attachments/827646208411303997/848534227648774144/unknown.png');
+            await interaction.reply('GS');
         }else if (arme === 'db'){
-            await interaction.reply('https://media.discordapp.net/attachments/827646415953068052/829804375752048730/DB_Foudre_Zinogre.png?width=1248&height=702');
+            await interaction.reply('DB');
         }
         
     }
